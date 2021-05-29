c37c2269c2966ce45d64dead89e31913442b6dd5
Lucas Toledo <ltoledo@wkm.com.br>
2021-05-29 20:24:59 -0300

    Crawler emec

---
+++ pages/api/emec/v1/detail/[ies].js
@@ -0,0 +1,70 @@
+
+async function tempo(request, response){
+  response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
+
+  const { ies } = request.query
+
+  const scrapy = require('node-scrapy')
+  const fetch = require('node-fetch')
+
+  let buff = new Buffer(ies);
+  let base64data = buff.toString('base64');
+
+
+  const url = 'https://emec.mec.gov.br/emec/consulta-ies/index/d96957f455f6405d14c6542552b0f6eb/'+base64data  //MjEzMg=='
+  const model = {
+    ies: '.tooltip ($ | trim)',
+    status: 'tr:nth-child(4) tr:nth-child(2) > td:nth-child(2) ($ | trim)',
+    cnpj: 'tr:nth-child(2) > td:nth-child(2) ($ | trim)',
+    naturezaJuridica: 'tr:nth-child(3) > td:nth-child(2) ($ | trim)',
+    representanteLegal: 'tr:nth-child(4) > td:nth-child(2) ($ | trim)',
+    endereco:{
+      endereco: 'tr:nth-child(4) tr:nth-child(3) > td:nth-child(2) ($ | trim)',
+      numero: 'tr:nth-child(4) tr:nth-child(3) > td:nth-child(4) ($ | trim)',
+      complemento: 'tr:nth-child(4) tr:nth-child(4) > td:nth-child(2) ($ | trim)',
+      cep: 'tr:nth-child(4) tr:nth-child(4) > td:nth-child(4) ($ | trim)',
+      bairro: 'tr:nth-child(5) > td:nth-child(2) ($ | trim)',
+      municipio: 'tr:nth-child(6) > td:nth-child(2) ($ | trim)',
+      uf: 'tr:nth-child(4) tr:nth-child(6) > td:nth-child(4) ($ | trim)'
+    },
+    telefone: 'tr:nth-child(7) > td:nth-child(2) ($ | trim)',
+    organizacaoAcademica: 'tr:nth-child(8) > td:nth-child(2) ($ | trim)',
+    site: 'tr:nth-child(8) > td:nth-child(4) ($ | trim)',
+    email: 'tr:nth-child(4) tr:nth-child(9) > td:nth-child(2) ($ | trim)',
+    categoriaAdministrativa: 'tr:nth-child(4) tr:nth-child(10) > td:nth-child(2) ($ | trim)',
+    reitorOuDirigentePrincipal: 'tr:nth-child(4) tr:nth-child(11) > td:nth-child(2) ($ | trim)', 
+    tipoCredenciamento: 'tr:nth-child(4) tr:nth-child(12) > td:nth-child(2) ($ | trim)',
+    indices:{
+     CI_conceitoInstitucional: {
+       valor: 'tr:nth-child(6) tr:nth-child(1) > td:nth-child(2) ($ | trim)',
+       ano: 'tr:nth-child(6) tr:nth-child(1) > td:nth-child(3) ($ | trim)',
+     },
+     CI_EAD_conceitoInstitucional: {
+       valor: 'tr:nth-child(6) tr:nth-child(2) > td:nth-child(2) ($ | trim)',
+       ano: 'tr:nth-child(6) tr:nth-child(2) > td:nth-child(3) ($ | trim)',
+     },
+     IGC_indiceGeralCurso: {
+       valor: 'tr:nth-child(6) tr:nth-child(3) > td:nth-child(2) ($ | trim)',
+       ano: 'tr:nth-child(6) tr:nth-child(3) > td:nth-child(3) ($ | trim)',
+     },
+     IGC_continuo: {
+       valor: 'tr:nth-child(6) tr:nth-child(4) > td:nth-child(2) ($ | trim)',
+       ano: 'tr:nth-child(6) tr:nth-child(4) > td:nth-child(3) ($ | trim)',
+     }
+    }
+  }
+  
+  fetch(url)
+    .then((res) => res.text())
+    .then((body) => {
+      response.json({
+        model: scrapy.extract(body, model),
+        emec_url: url
+      });
+    })
+    .catch(console.error)
+  
+  
+}
+
+export default tempo;
